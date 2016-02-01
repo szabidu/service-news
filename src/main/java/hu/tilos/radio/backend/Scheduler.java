7ecbda77830a4bf22343de35e8a223eef8359982
Elek, Márton <elek@users.noreply.github.com>
2016-02-01 23:03:50 +0100

    new: additional 3 minutes fragments in case of emergency

---
+++ src/main/java/hu/tilos/radio/backend/Scheduler.java
@@ -0,0 +1,73 @@
+package hu.tilos.radio.backend;
+
+import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class Scheduler {
+
+    public List<NewsBlock> getScheduledBlocks(LocalDate date) {
+        List<NewsBlock> result = new ArrayList<>();
+
+        if (date.getDayOfWeek() == DayOfWeek.SATURDAY) {
+            result.add(new NewsBlock("reggel", date.atTime(8, 55), 11 * 60 + 30));
+            result.add(new NewsBlock("este", date.atTime(20, 0), 11 * 60 + 30));
+        } else if (date.getDayOfWeek() == DayOfWeek.SUNDAY) {
+            result.add(new NewsBlock("reggel", date.atTime(8, 50), 11 * 60 + 30));
+            result.add(new NewsBlock("este", date.atTime(20, 0), 11 * 60 + 30));
+        } else if (date.getDayOfWeek() == DayOfWeek.MONDAY) {
+            result.add(new NewsBlock("orankenti1", date.atTime(5, 0), 3 * 60));
+            result.add(new NewsBlock("reggel", date.atTime(6, 48), 9 * 60 + 2 * 3 * 60));
+            result.add(new NewsBlock("orankenti2", date.atTime(8, 0), 3 * 60));
+            result.add(new NewsBlock("orankenti3", date.atTime(9, 57), 6 * 60));
+            result.add(new NewsBlock("orankenti3-potrovid", date.atTime(9, 57), 3 * 60));
+            result.add(new NewsBlock("orankenti4", date.atTime(11, 57), 6 * 60));
+            result.add(new NewsBlock("orankenti4-potrovid", date.atTime(11, 57), 3 * 60));
+            result.add(new NewsBlock("orankenti5", date.atTime(13, 28, 30), 3 * 60));
+            result.add(new NewsBlock("orankenti6", date.atTime(14, 57, 30), 6 * 60));
+            result.add(new NewsBlock("orankenti6-potrovid", date.atTime(14, 57, 30), 3 * 60));
+            result.add(new NewsBlock("orankenti7", date.atTime(16, 28, 30), 3 * 60));
+            result.add(new NewsBlock("orankenti8", date.atTime(17, 30), 3 * 60));
+            result.add(new NewsBlock("orankenti9", date.atTime(18, 30), 3 * 60));
+            result.add(new NewsBlock("este", date.atTime(19, 48), 12 * 60 + +2 * 3 * 30));
+            result.add(new NewsBlock("orankenti10", date.atTime(21, 00), 3 * 60));
+            result.add(new NewsBlock("orankenti11", date.atTime(22, 00), 3 * 60));
+        } else if (date.getDayOfWeek() == DayOfWeek.TUESDAY) {
+            result.add(new NewsBlock("orankenti1", date.atTime(5, 0), 3 * 60));
+            result.add(new NewsBlock("reggel", date.atTime(6, 48), 9 * 60 + 2 * 3 * 60));
+            result.add(new NewsBlock("orankenti2", date.atTime(8, 0), 3 * 60));
+            result.add(new NewsBlock("orankenti3", date.atTime(9, 57), 6 * 60));
+            result.add(new NewsBlock("orankenti3-potrovid", date.atTime(9, 57), 3 * 60));
+            result.add(new NewsBlock("orankenti4", date.atTime(11, 57), 6 * 60));
+            result.add(new NewsBlock("orankenti4-potrovid", date.atTime(11, 57), 3 * 60));
+            result.add(new NewsBlock("orankenti5", date.atTime(13, 57, 0), 6 * 60));
+            result.add(new NewsBlock("orankenti5-potrovid", date.atTime(13, 57, 0), 2 * 60));
+            result.add(new NewsBlock("orankenti6", date.atTime(14, 57, 30), 6 * 60));
+            result.add(new NewsBlock("orankenti6-potrovid", date.atTime(14, 57, 30), 3 * 60));
+            result.add(new NewsBlock("orankenti7", date.atTime(16, 28, 30), 3 * 60));
+            result.add(new NewsBlock("orankenti8", date.atTime(17, 57), 6 * 60));
+            result.add(new NewsBlock("orankenti8-potrovid", date.atTime(17, 57), 3 * 60));
+            result.add(new NewsBlock("este", date.atTime(19, 48), 12 * 60 + +2 * 3 * 30));
+            result.add(new NewsBlock("orankenti10", date.atTime(21, 00), 3 * 60));
+            result.add(new NewsBlock("orankenti11", date.atTime(22, 00), 3 * 60));
+        } else {
+            result.add(new NewsBlock("reggel", date.atTime(6, 48), 11 * 60 + 30));
+            result.add(new NewsBlock("este", date.atTime(19, 48), 11 * 60 + 30));
+            result.add(new NewsBlock("rovid-5", date.atTime(6, 0), 3 * 60));
+            result.add(new NewsBlock("rovid-6", date.atTime(6, 0), 3 * 60));
+            for (int i = 8; i < 19; i++) {
+                result.add(new NewsBlock("rovid-" + i, date.atTime(i, 0), 3 * 60));
+            }
+            for (int i = 20; i < 23; i++) {
+                result.add(new NewsBlock("rovid-" + i, date.atTime(i, 0), 3 * 60));
+            }
+        }
+        return result;
+
+    }
+
+}
