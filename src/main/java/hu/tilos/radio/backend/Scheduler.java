e2319ff3a637d9dbb8db0dbf581cdd3fa34e0c6b
Elek, Márton <elek@users.noreply.github.com>
2016-02-04 22:06:53 +0100

    fix: full scheduling for the weekend

--- src/main/java/hu/tilos/radio/backend/Scheduler.java
+++ src/main/java/hu/tilos/radio/backend/Scheduler.java
@@ -13,13 +13,7 @@ public class Scheduler {
     public List<NewsBlock> getScheduledBlocks(LocalDate date) {
         List<NewsBlock> result = new ArrayList<>();
 
-        if (date.getDayOfWeek() == DayOfWeek.SATURDAY) {
-            result.add(new NewsBlock("reggel", date.atTime(8, 55), 11 * 60 + 30));
-            result.add(new NewsBlock("este", date.atTime(20, 0), 11 * 60 + 30));
-        } else if (date.getDayOfWeek() == DayOfWeek.SUNDAY) {
-            result.add(new NewsBlock("reggel", date.atTime(8, 50), 11 * 60 + 30));
-            result.add(new NewsBlock("este", date.atTime(20, 0), 11 * 60 + 30));
-        } else if (date.getDayOfWeek() == DayOfWeek.MONDAY) {
+        if (date.getDayOfWeek() == DayOfWeek.MONDAY) {
             result.add(new NewsBlock("orankenti1", date.atTime(5, 0), 3 * 60));
             result.add(new NewsBlock("reggel", date.atTime(6, 48), 9 * 60 + 2 * 3 * 60));
             result.add(new NewsBlock("orankenti2", date.atTime(8, 0), 3 * 60));
