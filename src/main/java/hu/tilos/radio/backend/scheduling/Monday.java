4352c99c1dffb30af9dba77c774ee1daf2bb2f32
Elek, Márton <elek@users.noreply.github.com>
2016-02-13 23:53:01 +0100

    fix: Mo-Tue scheduling

--- src/main/java/hu/tilos/radio/backend/scheduling/Monday.java
+++ src/main/java/hu/tilos/radio/backend/scheduling/Monday.java
@@ -13,7 +13,7 @@ public class Monday implements BaseScheduling {
     public List<NewsBlock> createBlocks(LocalDate date, boolean odd) {
         BlockBuilder builder = new BlockBuilder(date)
                 .addBlock("reggel", LocalTime.of(5, 0), 12)
-                .doubleBlock(7)
+                .singleBlockAt(6,57)
                 .doubleBlock(8)
                 .doubleBlock(10)
                 .addShort("3perces-11-pot", 11, 0, 3)
@@ -25,7 +25,8 @@ public class Monday implements BaseScheduling {
         }
         builder.doubleBlock(15)
                 .doubleBlock(17)
-                .singleBlockAt(18, 30).withSelection("children")
+                .singleBlockSymmetric(18, 30)
+                .singleBlockAt(19, 30).withSelection("children")
                 .addBlock("este", LocalTime.of(19, 48), 9 + 3)
                 .doubleBlock(21)
                 .addShort("3perces-22-pot", 22, 0, 3);
